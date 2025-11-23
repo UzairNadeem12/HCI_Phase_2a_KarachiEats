71840815a3d45dc1abe56bd16dcb06ad87a2c700
AsnaFarooqui <a.farooqui.26918@khi.iba.edu.pk>
2025-11-23 20:06:20 +0500

    Card expiry fix v2

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -352,7 +352,7 @@ const Checkout = () => {
                     onChange={(e) => {
                       let value = e.target.value.replace(/\D/g, ""); 
 
-                      if (value.length > 2) {
+                      if (value.length >= 2) {
                         value = value.slice(0, 2) + "/" + value.slice(2, 4);
                       }
 
